7da58dc3a99fe34df78000821d592d828ea1f2f1
kuvarti <uzali.kafasi@gmail.com>
2024-03-03 03:31:39 +0300

    nameconversion

--- src/modules/user-two-fa/user-two-fa.module.ts
+++ src/modules/user-two-fa/user-two-fa.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserTwoFaService } from 'src/business/concrete/user-two-fa/user-two-fa.service';
 import { UserTwoFasController } from 'src/controllers/user-two-fas/user-two-fas.controller';
-import { UserTwoFADal } from 'src/dataAccess/concrete/userTwoFADal';
+import { UserTwoFADal } from 'src/dataAccess/concrete/userTwoFaDal';
 import { UserTwoFA } from 'src/entities/concrete/userTwoFa.entity';
 
 @Module({
